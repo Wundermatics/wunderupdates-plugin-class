892e94a5a0f1343635a4337dd5f7bbdd4e985dcd
Erik Torsner <erik@torgesta.com>
2024-08-06 19:49:10 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # wunderupdates-plugin-class
-The WunderUpdates PHP class for plugins
+
+The WunderUpdates PHP class for plugins. Visit [wunderupdates.com](https://wunderupdates.com) to learn more.
+
+## Instructions
+
+To integrate your plugin with WunderUpdates, you'll need to add the WunderUpdates PHP class to your plugin. This class
+is responsible for handling the communication between your plugin and the WunderUpdates API so that your plugin can
+check for updates, download new versions, and verify the license key.
+
+### Step 1: Download the class
+
+Download the latest version of `class-wp-updates.php` from the [GitHub
+repository](https://github.com/Wundermatics/wunderupdates-plugin-class).
+
+### Step 2: Copy the class to your plugin
+
+Copy the `class-wp-updates.php` file to your plugin directory. You can rename it or place it in the root of your plugin
+or in a subdirectory, whatever works best for you.
+
+### Step 3: Set a new class name
+
+To avoid conflicts with other plugins that may use the same class name, you should rename the class to something unique.
+We suggest naming the class to match your WunderUpdates account key and plugin slug, for example:
+
+```php
+class WP_Updates_abcde123_hello_world {
+    // ...rest of the class here.
+}
+``` 
+### Step 4: Configure the class
+
+To configure the class in your plugin, you need to include it and set some properties
+
+```php
+require_once __DIR__ . '/class-wp-updates.php';
+$updates = new WP_Updates_abcde123_hello_world( array(
+		'version'     => '1.0.0',      // The current version of the plugin.
+		'slug'        => 'hello-nick', // Plugin slug.
+		'full_path'   => __FILE__,     // Full path to the root plugin file.
+		'account_key' => 'DxirA2y6',   // Your WunderUpdates account key.
+	) );
+```
+
---
+++ class-wp-updates.php
@@ -0,0 +1,188 @@
+<?php
+
+class WP_Updates_CHANGE_THIS {
+	/**
+	 * Default WP Updates API URL.
+	 */
+	const UPDATE_BASE = 'https://d3ga1v40bd0qfl.cloudfront.net';
+
+	/**
+	 * API Cache Time.
+	 */
+	const CACHE_TIME = HOUR_IN_SECONDS * 12;
+
+	/**
+	 * Plugin properties in array format.
+	 *
+	 * @var array
+	 */
+	private $properties;
+
+
+	/**
+	 * Update class constructor.
+	 *
+	 * @param array $properties
+	 * 
+	 * @return void
+	 */
+	public function __construct( $properties ) {
+		$this->register( $properties );
+	}
+
+	/**
+	 * Register hooks.
+	 *
+	 * @param array $properties
+	 * 
+	 * @return void
+	 */
+	public function register( $properties ) {
+		$parts                   = explode( '/', $properties['full_path'] );
+		$properties['base_name'] = join( '/', array_slice( $parts, -2 ) );
+		$this->properties        = $properties;
+
+		add_filter( 'plugins_api', array( $this, 'filter_plugin_update_info' ), 20, 3 );
+		add_filter( 'site_transient_update_plugins', array( $this, 'filter_plugin_update_transient' ) );
+	}
+
+	/**
+	 * Filter the plugin update transient add our info to update notifications.
+	 *
+	 * @handles site_transient_update_plugins
+	 *
+	 * @param object $transient
+	 *
+	 * @return object
+	 */
+	public function filter_plugin_update_transient( $transient ) {
+		if ( empty( $transient->checked ) ) {
+			return $transient;
+		}
+
+		$result = $this->fetch_plugin_info();
+
+		if ( false === $result ) {
+			return $transient;
+		}
+
+		if ( version_compare( $this->properties['version'], $result->version, '<' ) ) {
+			$res                                 = $this->parse_plugin_info( $result );
+			$transient->response[ $res->plugin ] = $res;
+			$transient->checked[ $res->plugin ]  = $result->version;
+		}
+
+		return $transient;
+	}
+
+	/**
+	 * Filters the plugin update information.
+	 *
+	 * @param object $res
+	 * @param string $action
+	 * @param object $args
+	 *
+	 * @handles plugins_api
+	 * @return object
+	 */
+	public function filter_plugin_update_info( $res, $action, $args ) {
+		// Not our plugin slug.
+		if ( $this->properties['slug'] !== $args->slug ) {
+			return $res;
+		}
+
+		// Not the action we want to handle.
+		if ( 'plugin_information' !== $action ) {
+			return $res;
+		}
+
+		$result = $this->fetch_plugin_info();
+
+		// do nothing if we don't get the correct response from the server
+		if ( false === $result ) {
+			return $res;
+		}
+
+		return $this->parse_plugin_info( $result );
+	}
+
+	/**
+	 * Fetches the plugin update object from the server API.
+	 *
+	 * @return object|false
+	 */
+	private function fetch_plugin_info() {
+		//Fetch cache first
+		$transient_name = $this->get_transient_key();
+		$response       = get_transient( $transient_name );
+
+		if ( empty( $response ) ) {
+			$response = wp_remote_get(
+				sprintf(
+					'%s/public/%s/plugins/%s',
+					self::UPDATE_BASE,
+					$this->properties['account_key'],
+					$this->properties['slug']
+				),
+				array(
+					'timeout' => 10,
+					'headers' => array(
+						'Accept' => 'application/json',
+					),
+				)
+			);
+
+			if (
+				is_wp_error( $response ) ||
+				200 !== wp_remote_retrieve_response_code( $response ) ||
+				empty( wp_remote_retrieve_body( $response ) )
+			) {
+				return false;
+			}
+
+			$response = wp_remote_retrieve_body( $response );
+
+			//Cache the response
+			set_transient( $transient_name, $response, self::CACHE_TIME );
+		}
+
+		return json_decode( $response );
+	}
+
+	/**
+	 * Parses the product info response.
+	 *
+	 * @param object $response
+	 *
+	 * @return stdClass
+	 */
+	private function parse_plugin_info( $response ) {
+		$res                = new stdClass();
+		$res->name          = $response->plugin_name ?? '';
+		$res->slug          = $this->properties['slug'];
+		$res->version       = $response->version ?? '';
+		$res->requires      = $response->requires_at_least ?? '';
+		$res->requires_php  = $response->requires_php ?? '';
+		$res->download_link = $response->download_link ?? '';
+		$res->new_version   = $response->version ?? '';
+		$res->plugin        = $this->properties['base_name'];
+		$res->package       = $response->download_link ?? '';
+		$res->tested        = $response->tested_up_to ?? '';
+
+		$res->sections = array(
+			'description' => $response->sections->description ?? '',
+			'changelog'   => $response->sections->changelog ?? '',
+		);
+
+		return $res;
+	}
+
+	/**
+	 * Return the transient key for the plugin info response.
+	 *
+	 * @return string
+	 */
+	private function get_transient_key() {
+		return 'wpup_update_info_response_' . sanitize_title( $this->properties['slug'] );
+	}
+}
